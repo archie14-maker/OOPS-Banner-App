2a6d93c4d1a44f29e75af21e1e2b9908a8b8a8d9
archie14-maker <aaradhyaarchiee@gmail.com>
2026-03-19 03:58:59 +0530

    UC6:string[]
    Added methods to render and print characters for OOPS.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,45 @@
 public class OOPSBannerApp {
+
+    
+    public static void printCharacter(String[] pattern) {
+        for (String line : pattern) {
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+    public static void renderWord() {
+
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+
+        String[] P = {
+                "***** ",
+                "*    *",
+                "***** ",
+                "*     ",
+                "*     "
+        };
+
+        String[] S = {
+                " *****",
+                "*     ",
+                " *****",
+                "      *",
+                " *****"
+        };
+
+        printCharacter(O);
+        printCharacter(O);
+        printCharacter(P);
+        printCharacter(S);
+    }
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        renderWord();   // calling static function
     }
 }
