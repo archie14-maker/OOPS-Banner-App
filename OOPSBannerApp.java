217c004773c9046aa6a11294c0eae7b02e65b665
archie14-maker <aaradhyaarchiee@gmail.com>
2026-03-19 04:02:35 +0530

    UC8:new string
    Added methods to build a pattern map and render a banner for the word 'OOPS'.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,63 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
+    private static Map<Character, String[]> buildPatternMap() {
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        map.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        });
+
+        map.put('S', new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        });
+
+        return map;
+    }
+
+    private static void renderBanner(String word, Map<Character, String[]> patternMap) {
+        int rows = 7;
+
+        for (int row = 0; row < rows; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        Map<Character, String[]> patternMap = buildPatternMap();
+        renderBanner("OOPS", patternMap);
     }
 }
